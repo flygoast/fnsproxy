f89e75bd88a70955e54dba356cc2c36d426a8c4c
flygoast <flygoast@126.com>
2012-09-20 08:38:57 +0800

    delete version.h

--- version.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VERSION_H_INCLUDED__
-#define __VERSION_H_INCLUDED__
-
-#define FNSPROXY_VERSION    "0.0.3M 5005f8e"
-#define FNSPROXY_COPYRIGHT  \
-    "Copyright (C) 2012 by flygoast\n"  \
-    "flygoast@126.com\n"
-
-#endif /* __VERSION_H_INCLUDED__ */
